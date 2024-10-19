a26ede546aee13cdeba7f6fe92bbfd5f5a0738c8
Peter Motzko <info@pmoscode.de>
2024-10-19 23:31:39 +0200

    fix: fix wrong path resolving for dev

--- tasks/build.sh
+++ tasks/build.sh
@@ -3,11 +3,13 @@ set -e
 
 VERSIONS=$(ls -d */ | cut -f1 -d'/')
 VARIANT=$1
+CURRENT_PATH=$(pwd)
 
 for VERSION in $VERSIONS
 do
 	echo "Processing $VERSION..."
-	ABS_VERSION_FOLDER=$(realpath $VERSION)
+	ABS_VERSION_FOLDER=$CURRENT_PATH/$VERSION
+  echo "Going in to folder: $ABS_VERSION_FOLDER"
 	cd $ABS_VERSION_FOLDER
 
   apko build $VARIANT.yaml $ARTIFACT_NAME-$VERSION:$VARIANT $ARTIFACT_NAME-$VERSION-$VARIANT.tar
